8e639802c62531b78b2abe76ce56a7fe06fed196
Akshat Yadav <71210501+Akshat1276@users.noreply.github.com>
2024-10-12 23:08:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def sum(a, b, c ):
 def printBoard(xState, zState):
     zero = 'X' if xState[0] else ('O' if zState[0] else " ")
     one = 'X' if xState[1] else ('O' if zState[1] else " ")
-    two = 'X' if xState[2] else ('O' if zState[2] else " ")
+    two = 'X' if xState[2] else ('O' if zState[2] else " ") 
     three = 'X' if xState[3] else ('O' if zState[3] else " ")
     four = 'X' if xState[4] else ('O' if zState[4] else " ")
     five = 'X' if xState[5] else ('O' if zState[5] else " ")
